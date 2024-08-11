dc1f1660ccfbf1eaa65ff837947809abd72c01ee
Jose Shardin <joseruiz.sh@gmail.com>
2024-08-11 17:48:00 -0500

    app peliculas en React + Vite

---
+++ README.md
@@ -0,0 +1,5 @@
+# React + Vite
+
+Buscador de películas en react
+
+Link: [Netlify](https://app-react-movie-pe.netlify.app/)
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BuscadorDePeliculas } from "./BuscadorDePeliculas";
+import "./styles/buscador.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BuscadorDePeliculas />
+  </React.StrictMode>
+);
